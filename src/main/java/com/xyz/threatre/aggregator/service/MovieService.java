10f178370da17c755b6d70096356a4db24403c21
Bappa <bappabitjobs@gmail.com>
2025-09-21 14:36:06 +0100

    Code cleanup

--- src/main/java/com/xyz/threatre/aggregator/service/MovieService.java
+++
@@ -1,6 +0,0 @@
-package com.xyz.threatre.aggregator.service;
-
-public interface MovieService {
- //   String addMovie(MovieRequest movieRequest);
- //   String deleteMovie(MovieRequest movieRequest);
-}
